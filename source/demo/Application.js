0f29dc2348c158a4e04a60a76059bb1173c47a83
Lois Postula <lois.postula@gmail.com>
2017-01-03 17:03:05 +0100

    Initial commit

---
+++ source/demo/Application.js
@@ -0,0 +1,14 @@
+/**
+ Copyright (C) 03/01/17  Loïs Postula
+ */
+import React, { Component } from 'react'
+
+export default class Application extends Component {
+  render () {
+    return (
+      <div className='demo'>
+        Ceci est un message de test, s'affiche-t-il?
+      </div>
+    )
+  }
+}
---
+++ source/demo/index.js
@@ -0,0 +1,11 @@
+/**
+ Copyright (C) 03/01/17  Loïs Postula
+ */
+import React from 'react'
+import {render} from 'react-dom'
+import Application from './Application'
+
+render(
+  <Application />,
+  document.getElementById('root')
+)
