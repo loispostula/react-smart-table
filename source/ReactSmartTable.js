e23a4b623db93ef109f17c0d9fb8f9e974fc0f20
Lois Postula <lois.postula@gmail.com>
2017-01-03 17:56:26 +0100

    allow generation of github page

---
+++ source/ReactSmartTable.js
@@ -0,0 +1,13 @@
+/**
+ Copyright (C) 03/01/17  Loïs Postula
+ */
+import React, { Component } from 'react'
+export default class ReactSmartTable extends Component {
+  render () {
+    return (
+      <div>
+        Here will be the most important part of the table
+      </div>
+    )
+  }
+}
--- source/demo/Application.js
+++ source/demo/Application.js
@@ -2,20 +2,35 @@
  Copyright (C) 03/01/17  Loïs Postula
  */
 import React, { Component } from 'react'
+import styles from './demo.css'
+import {ReactSmartTable} from '../index'
 
 export default class Application extends Component {
   render () {
     return (
-      <div className='demo'>
-        <div className='header'>
-          <div className='logo'>
-            <img
-              alt="React Smart Table"
-              src=""
-            />
+      <div className={styles.demo}>
+        <div className={styles.headerRow}>
+          <div className={styles.logoRow}>
+            <div className={styles.Container}>
+              <img
+                alt='React Smart Table'
+                className={styles.logo}
+                src='https://raw.githubusercontent.com/loispostula/react-smart-table/master/source/static/logo.png'
+              />
+              <div className={styles.PrimaryLogoText}>
+                React
+              </div>
+              <div className={styles.SecondaryLogoText}>
+                Smart Table
+              </div>
+            </div>
           </div>
         </div>
-        Ceci est un message de test, s'affiche-t-il?
+        <div className={styles.body}>
+          <ReactSmartTable
+
+          />
+        </div>
       </div>
     )
   }
